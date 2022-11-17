e92393ff3c4603baf229085486ad468cf9f9e9b6
邱志壅 <jasonchiu@qiuzhiyongdeMacBook-Air.local>
2022-11-17 21:32:49 +0800

    3.9-純手工打造-005

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,8 +4,18 @@ class CandidatesController < ApplicationController
     end
 
     def new
+        @candidate = Candidate.new
     end
 
     def create
+        @candidate = Candidate.new(params[:candidate])
+        #上面的params[：candidate]是在debugger模式測出來的，利用params這個變數hash取得:candidate這包東西出來，然後透過new的方式丟給Candidate這個model
+        if @candidate.save
+            #如果成功，轉指導候選人列表列
+            redirect_to '/candidates'
+            #redirect_to是轉址的指令
+        else
+            #如果NG
+        end
     end
 end
